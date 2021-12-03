84a62128c1c25f07d340680c9d126da93659bcb7
Joni Van Roost <joni.vr@hotmail.com>
2021-12-03 13:04:46 +0100

    update example with actionsheet & use fix branch

--- components/SecondScreen.tsx
+++ components/SecondScreen.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ActionSheetIOS } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export const SecondScreen = () => {
   const navigation = useNavigation<any>();
 
+  const onPressActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Option 1', 'Option 2'],
+        destructiveButtonIndex: 2,
+        cancelButtonIndex: 0,
+      },
+      (buttonIndex) => console.log(buttonIndex)
+    );
+  };
+
   return (
     <View style={style.container}>
       <Text>Screen 2</Text>
+      <Button title="actionSheet" onPress={onPressActionSheet} />
       <Button title="close" onPress={() => navigation.goBack()} />
     </View>
   );
